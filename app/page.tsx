b76992918f82347777822777a5ea32871c0e84ec
Gauthier Amano <gauthier.net@gmail.com>
2025-08-28 19:26:55 +0200

    feat(app/page.tsx): ✨ use `Login`

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Login from "@/components/login";
+
 export default function Home() {
-  return <div className="grid min-h-screen p-8 font-sans sm:p-20" />;
+  return (
+    <div className="grid min-h-screen place-content-center p-8 font-sans sm:p-20">
+      <div className="flex justify-center sm:max-w-lg">
+        <Login />
+      </div>
+    </div>
+  );
 }

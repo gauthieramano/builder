d89c27fd9b89fc0edabbf4406b4c304c1a7a0f5c
Gauthier Amano <gauthier.net@gmail.com>
2025-08-31 17:03:07 +0200

    feat(app & components): ✨ use localStorage to load & store LLM API key

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type { PropsLlmApiKey } from "@/components/local-storage-llm-api-key";
 import Login from "@/components/login";
 
+const LocalStorageLlmApiKey = dynamic(
+  () => import("@/components/local-storage-llm-api-key"),
+  {
+    loading: () => <Login llmApiKey="" />,
+    ssr: false,
+  },
+);
+
 export default function Home() {
+  const renderLogin = (props: PropsLlmApiKey) => <Login {...props} />;
+
   return (
     <div className="grid min-h-screen place-content-center p-8 font-sans sm:p-20">
       <div className="flex justify-center sm:max-w-lg">
-        <Login />
+        <LocalStorageLlmApiKey render={renderLogin} />
       </div>
     </div>
   );

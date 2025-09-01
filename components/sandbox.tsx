b957fa6df1155e60b708b74453867b23a92b67c1
Gauthier Amano <gauthier.net@gmail.com>
2025-09-01 07:41:27 +0200

    feat(app & components): ✨ add `Sandbox` with a React application

---
+++ components/sandbox.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  SandpackLayout,
+  SandpackPreview,
+  SandpackProvider,
+} from "@codesandbox/sandpack-react";
+
+const DEFAULT_CODE = `import React from "react";
+import { Edit } from "lucide-react";
+import ReactMarkdown from "react-markdown";
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-indigo-500 from-5% via-sky-500 via-40% to-emerald-500 to-90% text-white">
+      <div className="text-4xl">Hello, world!</div>
+      <div className="flex items-center gap-2">
+        <Edit size={24} />
+        <ReactMarkdown>*This is **Markdown***</ReactMarkdown>
+      </div>
+    </div>
+  );
+}
+`;
+
+const HTML = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Builder Sandbox</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+`;
+
+type Props = { code?: string };
+
+export default function Sandbox({ code }: Props) {
+  return (
+    <main className="min-w-lg grow">
+      <div className="overflow-hidden rounded-lg border">
+        <SandpackProvider
+          template="react"
+          options={{ externalResources: ["https://cdn.tailwindcss.com"] }}
+          customSetup={{
+            dependencies: {
+              "lucide-react": "0.542.0",
+              "react-markdown": "9.0.1",
+            },
+          }}
+          files={{
+            "/App.js": code || DEFAULT_CODE,
+            "public/index.html": HTML,
+          }}
+        >
+          <SandpackLayout className="border-0!">
+            <SandpackPreview className="h-[calc(100dvh-112px)]! sm:h-[calc(100dvh-160px)]!" />
+          </SandpackLayout>
+        </SandpackProvider>
+      </div>
+    </main>
+  );
+}
--- components/vibecoder.tsx
+++ components/vibecoder.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import Chat from "./chat";
 import type { PropsLlmApiKey } from "./local-storage-llm-api-key";
+import Sandbox from "./sandbox";
 
 export default function Vibecoder({ llmApiKey }: PropsLlmApiKey) {
   const hasLlmApiKey = !!llmApiKey;
@@ -9,5 +10,10 @@ export default function Vibecoder({ llmApiKey }: PropsLlmApiKey) {
     redirect("/");
   }
 
-  return <Chat llmApiKey={llmApiKey} />;
+  return (
+    <div className="flex flex-row-reverse gap-2">
+      <Sandbox />
+      <Chat llmApiKey={llmApiKey} />
+    </div>
+  );
 }

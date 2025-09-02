7729a1eeddbb802143dc20583b1433a94460331d
Gauthier Amano <gauthier.net@gmail.com>
2025-09-02 06:16:36 +0200

    feat(app & components): ✨ display Chat errors like API key-related ones

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: Props) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   );

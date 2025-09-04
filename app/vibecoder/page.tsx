45a440a86a796000e38ce877a51d57e64d86d259
Gauthier Amano <gauthier.net@gmail.com>
2025-09-05 00:46:21 +0200

    feat(app/vibecoder/page.tsx): ✨ add button to log out

--- app/vibecoder/page.tsx
+++ app/vibecoder/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { X } from "lucide-react";
 import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
 import type { PropsLlmApiKey } from "@/components/local-storage-llm-api-key";
 import { SkeletonCard } from "@/components/skeleton-card";
+import { Button } from "@/components/ui/button";
 import Vibecoder from "@/components/vibecoder";
 
 const LocalStorageLlmApiKey = dynamic(
@@ -16,10 +19,24 @@ const LocalStorageLlmApiKey = dynamic(
 export default function VidecoderPage() {
   const renderVibecoder = (props: PropsLlmApiKey) => <Vibecoder {...props} />;
 
+  const logout = () => {
+    window.localStorage.clear();
+    redirect("/");
+  };
+
   return (
     <div className="grid min-h-screen px-4 pt-12 font-sans md:px-12">
-      <div>
+      <div className="relative">
         <LocalStorageLlmApiKey render={renderVibecoder} />
+
+        <Button
+          onClick={logout}
+          size="icon"
+          variant="ghost"
+          className="-top-10 absolute right-0 size-8"
+        >
+          <X />
+        </Button>
       </div>
     </div>
   );
